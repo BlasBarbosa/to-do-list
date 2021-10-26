f96f1de2ec3ec9e3892b30d5c54cb055bd0fa70b
BlasBarbosa <91706860+BlasBarbosa@users.noreply.github.com>
2021-10-26 17:01:48 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
+
+function App() {
+  return (
+    <div className="todo-app">
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
+
+//enlace para continuar con el proyecto... nos quedamos en el minuto 16:00...
+//   https://www.youtube.com/watch?v=E1E08i2UJGI
